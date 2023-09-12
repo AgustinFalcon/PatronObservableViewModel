e7bf86dc50030d613c42551de6eff2b9a485e317
Agustin Falcon <agustinrobertofalcon@gmail.com>
2023-09-12 15:11:05 -0300

    Patron observable utilizando estados y material design

---
+++ app/src/main/java/com/example/patronobservable/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.patronobservable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
+import com.example.patronobservable.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+
+        viewModel.viewState.observe(this) {
+            handleViewState(it)
+        }
+
+
+        binding.txtEmail.addTextChangedListener {
+            viewModel.validateEmail(it.toString())
+        }
+
+        binding.txtPassword.addTextChangedListener {
+            viewModel.validatePass(it.toString())
+        }
+
+        binding.txtRepeatPass.addTextChangedListener {
+            viewModel.validateRepeatPass(it.toString())
+        }
+
+    }
+
+
+
+    private fun handleViewState(viewState: RegisterStates) {
+        when (viewState) {
+            is RegisterStates.SuccessEmailField -> binding.layoutEmail.error = null
+
+            is RegisterStates.ErrorEmailField -> binding.layoutEmail.error = viewState.email
+
+            is RegisterStates.SuccessPassField -> binding.layoutPassword.error = null
+
+            is RegisterStates.ErrorPassField -> binding.layoutPassword.error = "Mínimo: ${viewState.password.length}/3"
+
+            is RegisterStates.SuccessRepeatPassField -> binding.layoutRepeatPass.error = null
+
+            is RegisterStates.ErrorRepeatPassField -> binding.layoutRepeatPass.error = viewState.message
+
+            is RegisterStates.BtnSuccess -> binding.btnRegister.isEnabled = true
+            is RegisterStates.BtnError -> binding.btnRegister.isEnabled = false
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/patronobservable/MainViewModel.kt
@@ -0,0 +1,75 @@
+package com.example.patronobservable
+
+import androidx.core.util.PatternsCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+
+class MainViewModel: ViewModel() {
+
+
+    private val _viewState = MutableLiveData<RegisterStates>()
+    val viewState: LiveData<RegisterStates> get() = _viewState
+
+    private var email: String = ""
+    private var password: String = ""
+    private var repeatPass: String = ""
+
+
+    private fun validateButtons() {
+        if (PatternsCompat.EMAIL_ADDRESS.matcher(email).matches() && (password == repeatPass && password.isNotEmpty() && repeatPass.isNotEmpty())) {
+            _viewState.value = RegisterStates.BtnSuccess
+        } else {
+            _viewState.value = RegisterStates.BtnError
+        }
+    }
+
+
+    fun validateEmail(email: String) {
+        if (email.isNotEmpty() && PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()) {
+            _viewState.value = RegisterStates.SuccessEmailField
+            this.email = email
+        } else {
+            _viewState.value = RegisterStates.ErrorEmailField(email = "Ingrese un email valido")
+        }
+        validateButtons()
+    }
+
+
+    fun validatePass(password: String) {
+        if (password.isNotEmpty() && password.length >= 3) {
+            _viewState.value = RegisterStates.SuccessPassField
+            this.password = password
+        } else {
+            _viewState.value = RegisterStates.ErrorPassField(password = password)
+        }
+        validateButtons()
+    }
+
+
+    fun validateRepeatPass(repeatPass: String) {
+        if (password == repeatPass) {
+            _viewState.value = RegisterStates.SuccessRepeatPassField
+            this.repeatPass = repeatPass
+        } else {
+            _viewState.value = RegisterStates.ErrorRepeatPassField(message = "Las contraseñas son distintas")
+        }
+        validateButtons()
+    }
+
+
+}
+
+
+sealed class RegisterStates() {
+    object SuccessEmailField: RegisterStates()
+    data class ErrorEmailField(val email: String): RegisterStates()
+    object SuccessPassField: RegisterStates()
+    data class ErrorPassField(val password: String): RegisterStates()
+    object SuccessRepeatPassField: RegisterStates()
+    data class ErrorRepeatPassField(val message: String): RegisterStates()
+    object BtnSuccess: RegisterStates()
+    object BtnError: RegisterStates()
+}
